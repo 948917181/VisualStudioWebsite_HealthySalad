622313cd7231f32e7f658d7b692cc44039cdbb7f
948917181 <42567722+948917181@users.noreply.github.com>
2018-10-20 18:11:55 +1100

    Add files via upload

---
+++ FIT5032/Controllers/RecipesController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FIT5032.Models;
+
+namespace FIT5032.Controllers
+{
+    public class RecipesController : Controller
+    {
+        private FIT5032_ModelsContainer db = new FIT5032_ModelsContainer();
+
+        // GET: Recipes
+        public ActionResult Index()
+        {
+            return View(db.Recipes.ToList());
+        }
+
+        // GET: Recipes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Recipe recipe = db.Recipes.Find(id);
+            if (recipe == null)
+            {
+                return HttpNotFound();
+            }
+            return View(recipe);
+        }
+
+        // GET: Recipes/Create
+        [Authorize(Roles = "customer")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Recipes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "customer")]
+        public ActionResult Create([Bind(Include = "Id,RecipeTitle,Author,PublishDate,Content")] Recipe recipe)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Recipes.Add(recipe);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+                return View(recipe);
+            }
+            catch {
+                ViewBag.ErrorMessage = "Required fields cannot be blank!";
+                return View(recipe);
+            }
+            
+        }
+
+        // GET: Recipes/Edit/5
+        [Authorize(Roles = "admin")]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Recipe recipe = db.Recipes.Find(id);
+            if (recipe == null)
+            {
+                return HttpNotFound();
+            }
+            return View(recipe);
+        }
+
+        // POST: Recipes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "admin")]
+        public ActionResult Edit([Bind(Include = "Id,RecipeTitle,Author,PublishDate,Content")] Recipe recipe)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Entry(recipe).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(recipe);
+            }
+            catch {
+                ViewBag.ErrorMessage = "Required fields cannot be blank!";
+                return View(recipe);
+            }
+            }
+
+        // GET: Recipes/Delete/5
+        [Authorize(Roles = "admin")]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Recipe recipe = db.Recipes.Find(id);
+            if (recipe == null)
+            {
+                return HttpNotFound();
+            }
+            return View(recipe);
+        }
+
+        // POST: Recipes/Delete/5
+        [Authorize(Roles = "admin")]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Recipe recipe = db.Recipes.Find(id);
+            db.Recipes.Remove(recipe);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ FIT5032/Controllers/ReviewsController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FIT5032.Models;
+
+namespace FIT5032.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        private FIT5032_ModelsContainer db = new FIT5032_ModelsContainer();
+
+        // GET: Reviews
+        public ActionResult Index()
+        {
+            var reviews = db.Reviews.Include(r => r.Recipe);
+            return View(reviews.ToList());
+        }
+
+        // GET: Reviews/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Review review = db.Reviews.Find(id);
+            if (review == null)
+            {
+                return HttpNotFound();
+            }
+            return View(review);
+        }
+
+        // GET: Reviews/Create
+        [Authorize(Roles = "reviewer")]
+        public ActionResult Create()
+        {
+            ViewBag.RecipeId = new SelectList(db.Recipes, "Id", "RecipeTitle");
+            return View();
+        }
+
+        // POST: Reviews/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "reviewer")]
+        public ActionResult Create([Bind(Include = "Id,Reviewer,PublishDate,Content,RecipeId")] Review review)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Reviews.Add(review);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+                ViewBag.RecipeId = new SelectList(db.Recipes, "Id", "RecipeTitle", review.RecipeId);
+                return View(review);
+            }
+            catch
+            {
+                ViewBag.ErrorMessage = "Required fields cannot be Blank!";
+                return View(review);
+            }
+        }
+
+        // GET: Reviews/Edit/5
+        [Authorize(Roles = "admin")]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Review review = db.Reviews.Find(id);
+            if (review == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.RecipeId = new SelectList(db.Recipes, "Id", "RecipeTitle", review.RecipeId);
+            return View(review);
+        }
+
+        // POST: Reviews/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "admin")]
+        public ActionResult Edit([Bind(Include = "Id,Reviewer,PublishDate,Content,RecipeId")] Review review)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Entry(review).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                ViewBag.RecipeId = new SelectList(db.Recipes, "Id", "RecipeTitle", review.RecipeId);
+                return View(review);
+            }
+            catch
+            {
+                ViewBag.ErrorMessage = "Required fields cannot be blank!";
+                return View(review);
+            }
+        }
+
+        // GET: Reviews/Delete/5
+        [Authorize(Roles = "admin")]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Review review = db.Reviews.Find(id);
+            if (review == null)
+            {
+                return HttpNotFound();
+            }
+            return View(review);
+        }
+
+        // POST: Reviews/Delete/5
+        [Authorize(Roles = "admin")]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Review review = db.Reviews.Find(id);
+            db.Reviews.Remove(review);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
